9a66716c48a8f6346b7df7020d24cbbfa85a3667
mhhussain <2152584+mhhussain@users.noreply.github.com>
2024-08-10 01:26:00 -0400

    .

---
+++ lib/data/api/local/ingredient_api.dart
@@ -0,0 +1,10 @@
+import 'package:pantrythief/data/models/ingredient_model.dart';
+
+class IngredientApi {
+  Future<List<IngredientModel>> getIngredients() async {
+    // retrieve list from shared preferences
+    // decode each string from base64 -> jsonstring
+    // json decode and map with fromJson methods on models
+    return [];
+  }
+}
--- lib/data/models/recipe_model.dart
+++ lib/data/models/recipe_model.dart
@@ -1,3 +1,4 @@
+import 'package:pantrythief/data/models/ingredient_model.dart';
 import 'package:pantrythief/domain/entities/recipes_entity.dart';
 
 class RecipeModel extends RecipeEntity {
@@ -10,7 +11,8 @@ class RecipeModel extends RecipeEntity {
   factory RecipeModel.fromJson(Map<String, dynamic> map) {
     return RecipeModel(
       name: map['name'],
-      ingredients: map['ingredients'],
+      // ingredient data must be extracted from inner json objects
+      ingredients: (map['ingredients'] as List).map((i) => IngredientModel.fromJson(i)).toList(),
       instructions: map['instructions'],
     );
   }
